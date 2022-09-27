3636d49b9ec4d768d2d2e3466a305572c759f9a1
Dayan <m.dayan5689@gmail.com>
2022-09-26 21:03:45 -0700

    Finalized code and took care of error handling

--- script.js
+++ script.js
@@ -7,27 +7,26 @@ function getComputerChoice() {
 function playRound(computerChoice, playerChoice) {
   computerChoice = getComputerChoice();
   playerChoice = prompt("Enter your choice: ").toLowerCase();
-  console.log(playerChoice);
   if (
     ((playerChoice == "rock") && (computerChoice == "scissors")) ||
     ((playerChoice == "paper") && (computerChoice == "rock")) ||
     ((playerChoice == "scissors") && (computerChoice == "paper"))
   ) {
-    console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+    return (`You win! ${playerChoice} beats ${computerChoice}`);
   } else if (
     ((playerChoice == "paper") && (computerChoice == "scissors")) ||
     ((playerChoice == "scissors") && (computerChoice == "rock")) ||
     ((playerChoice == "rock") && (computerChoice == "paper"))
   ) {
-    console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+    return (`You lose! ${computerChoice} beats ${playerChoice}`);
   }
+  return "Invalid choice"
 }
 
 function play() {
-  for (let i = 0; i <= 5; i++) {
-    playRound();
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound());
   }
 }
 
-console.log(choices);
 play();
